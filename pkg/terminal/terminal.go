3f4d1020a6e468da5ce8883494a979d801520a03
Rômulo von Santos <rromulosp@gmail.com>
2023-03-05 19:54:10 -0300

    adds terminal package

---
+++ pkg/terminal/terminal.go
@@ -0,0 +1,26 @@
+package terminal
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func CleanTerminal() {
+	switch runtime.GOOS {
+	case "darwin":
+		runCmd("clear")
+	case "linux":
+		runCmd("clear")
+	case "windows":
+		runCmd("cmd", "/c", "cls")
+	default:
+		runCmd("clear")
+	}
+}
+
+func runCmd(name string, arg ...string) {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
