a1ff5b05cc2f900d7619d48b41151f1aac4e2740
Rômulo von Santos <rromulosp@gmail.com>
2023-03-05 19:53:56 -0300

    creates methods for the main menu

--- internal/userInteraction.go
+++ internal/userInteraction.go
@@ -4,11 +4,79 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/rromulos/aps-finder/pkg/terminal"
 )
 
-func EnableVerboseMode() string {
-	fmt.Print("Do you want to enable the verbose mode? (y/n) ")
+//shows the main menu
+func ShowMainMenu() {
+	fmt.Println("1 - Setup")
+	fmt.Println("2 - Search for App Settings")
+	fmt.Println("3 - Compare App Settings")
+	fmt.Println("4 - Start Web Service")
+	fmt.Println("5 - Close Application")
+	fmt.Println("----------------------------")
+	fmt.Print("Chose your option :")
+
 	input := bufio.NewScanner(os.Stdin)
 	input.Scan()
-	return input.Text()
+
+	for !validateChosenMenuIsValid(input.Text()) {
+		terminal.CleanTerminal()
+		break
+	}
+
+	redirectToChosenMenu(input.Text())
+}
+
+//checks if the option chosen in the main menu is valid
+func validateChosenMenuIsValid(chosenOption string) bool {
+	return contains(chosenOption)
+}
+
+//checks if the chosen options is a valid option
+func contains(str string) bool {
+	validMenuOptions := []string{"1", "2", "3", "4", "5"}
+	for _, s := range validMenuOptions {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
+//redirects to the chosen menu
+func redirectToChosenMenu(chosenOption string) {
+	num, err := strconv.Atoi(chosenOption)
+
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	switch num {
+	case 1:
+		fmt.Println("One")
+	case 2:
+		menuSearchForAppSettings()
+	case 3:
+		fmt.Println("Three")
+	default:
+		ShowMainMenu()
+	}
+}
+
+//Starts the process to search for app settings
+func menuSearchForAppSettings() {
+	for {
+		fmt.Print("Do you want to enable the verbose mode? (y/n) ")
+		input := bufio.NewScanner(os.Stdin)
+		input.Scan()
+		verboseMode = input.Text()
+		if verboseMode == "y" || verboseMode == "n" {
+			break
+		}
+	}
+	PerformAnalysis(verboseMode)
 }
